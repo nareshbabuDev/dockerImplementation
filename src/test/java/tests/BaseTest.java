02977e6ca48951b056b7643681113a7f97ff3854
naresh <nareshlearnway@gmail.com>
2022-03-08 16:17:55 +0530

    chrome options

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,6 +5,7 @@ import java.net.URL;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
@@ -19,16 +20,20 @@ public class BaseTest {
 		//System.setProperty("webdriver.chrome.driver","C:\\Users\\nares\\Documents\\Eclipse\\TestDockerImplementation\\drivers\\chromedriver.exe");
 		//driver = new ChromeDriver();	
 		String host = "localhost";
-		DesiredCapabilities capabilities = null;
-		capabilities = DesiredCapabilities.chrome();
-		if(System.getProperty("BROWSER")!=null && System.getProperty("BROWSER").equalsIgnoreCase("firefox")) {
-			capabilities = DesiredCapabilities.firefox();
-		}
 		if(System.getProperty("HUB_HOST")!=null){
 			host = System.getProperty("HUB_HOST");
 		}
 		String completeURL = "https://" + host + ":4444/wd/hub";
-		this.driver = new RemoteWebDriver(new URL(completeURL),capabilities);
+		DesiredCapabilities capabilities = null;
+		//capabilities = DesiredCapabilities.chrome();
+		if(System.getProperty("BROWSER")!=null && System.getProperty("BROWSER").equalsIgnoreCase("firefox")) {
+			capabilities = DesiredCapabilities.firefox();
+			this.driver = new RemoteWebDriver(new URL(completeURL),capabilities);
+		}else {
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("disable-infobars");
+			this.driver = new RemoteWebDriver(new URL(completeURL),options);
+		}
 	}
 	
 	@AfterTest
